ba53ac5707e8f0db6cdc22695a21a220e3bd0516
Roberto <roberto@losgimenez.com>
2025-06-14 13:25:13 +0100

    Last changes

--- source/main.cpp
+++ source/main.cpp
@@ -1,12 +1,18 @@
 #include "sudokuClass.hpp"
+#include <chrono>
 
 int main() {
-    Sudoku sudoku;
-    std::cout << "Sudoku Input: \n";
-    sudoku.print();
+  Sudoku sudoku;
+  std::cout << "Sudoku Input: \n";
+  sudoku.print();
+  auto now = std::chrono::steady_clock::now();
+  sudoku.solve();
+  auto then = std::chrono::steady_clock::now();
 
-    sudoku.solve();
-
-    std::cout << "Sudoku Solved: \n";
-    sudoku.print();
+  std::cout << "Sudoku Solved: \n";
+  sudoku.print();
+  std::cout << "Time difference = "
+            << std::chrono::duration_cast<std::chrono::milliseconds>(then - now)
+                   .count()
+            << "[µs]" << std::endl;
 }
